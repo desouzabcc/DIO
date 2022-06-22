5d8c34f600731ab9bc03c806476deff2381a0c1b
desouzabcc <desouzabcc@gmail.com>
2022-06-22 20:19:20 -0300

    first commit

---
+++ src/Conta.java
@@ -0,0 +1,71 @@
+
+public abstract class Conta implements IConta {
+	
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+
+	protected int agencia;
+	protected int numero;
+	protected double saldo;
+	protected Cliente cliente;
+	protected double taxa;
+	protected double rendimento;
+	
+	public Conta(Cliente cliente) {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
+	}
+
+	@Override
+	public void sacar(double valor) {
+		saldo -= valor;
+	}
+
+	@Override
+	public void depositar(double valor) {
+		saldo += valor;
+	}
+
+	@Override
+	public void transferir(double valor, IConta contaDestino) {
+		this.sacar(valor);
+		contaDestino.depositar(valor);
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public double getRendimento() {
+		return rendimento;
+	}
+
+	public void setRendimento(double valor){
+		rendimento = valor;
+	}
+
+	public double getTaxa() {
+		return taxa;
+	}
+	public void setTaxa(double valor){
+		taxa = valor;
+	}
+
+	protected void imprimirInfosComuns() {
+		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+		System.out.println(String.format("Agencia: %d", this.agencia));
+		System.out.println(String.format("Numero: %d", this.numero));
+		System.out.println(String.format("Saldo: %.2f", this.saldo));
+		System.out.println(String.format("Taxa: %.2f", this.taxa));
+		System.out.println(String.format("Rendimento: %.2f", this.rendimento));
+	}
+}
